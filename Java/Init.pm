e5a92d0c9c72e4d9a13da2392b47d8a7787948be
patrick <>
2001-03-27 20:27:26 +0000

    chaged stuff around a bit in order to make the methods callable later with JNI

--- Java/Init.pm
+++ Java/Init.pm
@@ -57,6 +57,13 @@ public class InlineJavaServer {
 	public HashMap objects = new HashMap() ;
 	public int objid = 1 ;
 
+	// This constructor is used in JNI mode
+	InlineJavaServer(boolean d) {
+		debug = d ;
+	}
+
+
+	// This constructor is used in server mode
 	InlineJavaServer(String[] argv) {
 		String mode = argv[0] ;
 		debug = new Boolean(argv[1]).booleanValue() ;
@@ -78,26 +85,10 @@ public class InlineJavaServer {
 
 				while (true){
 					String cmd = br.readLine() ;
-					debug("  packet recv is " + cmd) ;
 
-					if (cmd != null){
-						InlineJavaProtocol ijp = new InlineJavaProtocol(this, cmd) ;
-						try {
-							ijp.Do() ;
-							debug("  packet sent is " + ijp.response) ;
-							bw.write(ijp.response + "\n") ;
-							bw.flush() ;					
-						}
-						catch (InlineJavaException e){
-							String err = "error scalar:" + ijp.unpack(e.getMessage()) ;
-							debug("  packet sent is " + err) ;
-							bw.write(err + "\n") ;
-							bw.flush() ;
-						}
-					}
-					else{
-						System.exit(1) ;
-					}
+					String resp = ProcessCommand(cmd) ;
+					bw.write(resp) ;
+					bw.flush() ;
 				}
 			}
 			catch (IOException e){
@@ -109,6 +100,31 @@ public class InlineJavaServer {
 			System.err.println("Invalid startup mode " + mode) ;
 			System.exit(1) ;
 		}
+	}
+
+
+	public String ProcessCommand(String cmd){
+		debug("  packet recv is " + cmd) ;
+
+		String resp = null ;
+		if (cmd != null){
+			InlineJavaProtocol ijp = new InlineJavaProtocol(this, cmd) ;
+			try {
+				ijp.Do() ;
+				debug("  packet sent is " + ijp.response) ;
+				resp = ijp.response + "\n" ;
+			}
+			catch (InlineJavaException e){
+				String err = "error scalar:" + ijp.unpack(e.getMessage()) ;
+				debug("  packet sent is " + err) ;
+				resp = err + "\n" ;
+			}
+		}
+		else{
+			System.exit(1) ;
+		}
+
+		return resp ;
 	}
 
 
@@ -125,13 +141,28 @@ public class InlineJavaServer {
 			File dat = new File(module + ".jdat") ;
 			PrintWriter pw = new PrintWriter(new FileWriter(dat)) ;
 
+			String data = ProcessReport(class_list, idx) ;
+			pw.print(data) ; 
+			pw.close() ;
+		}
+		catch (IOException e){
+			System.err.println("Problems writing to " + module + ".jdat file: " + e.getMessage()) ;
+			System.exit(1) ;			
+		}
+	}
+
+
+	String ProcessReport(String [] class_list, int idx){
+		StringBuffer pw = new StringBuffer() ;
+
+		try {
 			for (int i = idx ; i < class_list.length ; i++){
 				if (! class_list[i].startsWith("InlineJavaServer")){
 					StringBuffer name = new StringBuffer(class_list[i]) ;
 					name.replace(name.length() - 6, name.length(), "") ;
 					Class c = Class.forName(name.toString()) ;
 															
-					pw.println("class " + c.getName()) ;
+					pw.append("class " + c.getName() + "\n") ;
 					Constructor constructors[] = c.getConstructors() ;
 					Method methods[] = c.getMethods() ;
 					Field fields[] = c.getFields() ;
@@ -140,35 +171,31 @@ public class InlineJavaServer {
 						Constructor x = constructors[j] ;
 						String sign = CreateSignature(x.getParameterTypes()) ;
 						Class decl = x.getDeclaringClass() ;
-						pw.println("constructor" + " " + sign) ;
+						pw.append("constructor" + " " + sign + "\n") ;
 					}
 					for (int j = 0 ; j < methods.length ; j++){
 						Method x = methods[j] ;
 						String stat = (Modifier.isStatic(x.getModifiers()) ? " static " : " instance ") ;
 						String sign = CreateSignature(x.getParameterTypes()) ;
 						Class decl = x.getDeclaringClass() ;
-						pw.println("method" + stat + decl.getName() + " " + x.getName() + sign) ;
+						pw.append("method" + stat + decl.getName() + " " + x.getName() + sign + "\n") ;
 					}
 					for (int j = 0 ; j < fields.length ; j++){
 						Field x = fields[j] ;
 						String stat = (Modifier.isStatic(x.getModifiers()) ? " static " : " instance ") ;
 						Class decl = x.getDeclaringClass() ;
 						Class type = x.getType() ;
-						pw.println("field" + stat + decl.getName() + " " + x.getName() + " " + type.getName()) ;
+						pw.append("field" + stat + decl.getName() + " " + x.getName() + " " + type.getName() + "\n") ;
 					}					
 				}
 			}
-
-			pw.close() ; 
-		}
-		catch (IOException e){
-			System.err.println("Problems writing to " + module + ".jdat file: " + e.getMessage()) ;
-			System.exit(1) ;			
 		}
 		catch (ClassNotFoundException e){
 			System.err.println("Can't find class: " + e.getMessage()) ;
 			System.exit(1) ;
 		}
+
+		return pw.toString() ;
 	}
 
 
@@ -188,6 +215,14 @@ public class InlineJavaServer {
 	}
 
 
+	public void debug(String s) {
+		if (debug){
+			System.err.println("java: " + s) ;
+			System.err.flush() ;
+		}
+	}
+
+
 	/*
 		Startup
 	*/
@@ -196,11 +231,10 @@ public class InlineJavaServer {
 	}
 
 
-	public void debug(String s) {
-		if (debug){
-			System.err.println("java: " + s) ;
-		}
+	public static InlineJavaServer jni_main(boolean debug) {
+		return new InlineJavaServer(debug) ;
 	}
+
 
 	<INLINE_JAVA_OBJECT>
 
