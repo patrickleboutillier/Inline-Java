9521b3ed6b64c38b4f96a064ea34532cba105414
patrick_leb <>
2003-11-30 21:53:10 +0000

    move to org/perl/inline/java

--- Java/sources/InlineJavaCastException.java
+++
@@ -1,7 +0,0 @@
-package org.perl.inline.java ;
-
-class InlineJavaCastException extends InlineJavaException {
-	InlineJavaCastException(String m){
-		super(m) ;
-	}
-}
--- Java/sources/InlineJavaException.java
+++
@@ -1,7 +0,0 @@
-package org.perl.inline.java ;
-
-public class InlineJavaException extends Exception { 
-	public InlineJavaException(String s) {
-		super(s) ;
-	}
-}
